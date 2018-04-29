b9bf18b47d1a3214ac0c556352bf57089569767f
Hugo Trippaers <hugo@trippaers.nl>
2018-04-29 13:33:24 +0200

    Initial Commit

---
+++ Bergjes2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bergjes2018
+//
+//  Created by Hugo Trippaers on 29/04/2018.
+//  Copyright © 2018 Hugo Trippaers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
